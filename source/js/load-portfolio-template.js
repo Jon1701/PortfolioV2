f0c72a6ff8cd989f334a439f5526100e16404811
Jon Balon <development@jonbalon.com>
2016-06-17 23:53:12 -0400

    Added dummy text to template data

--- source/js/load-portfolio-template.js
+++ source/js/load-portfolio-template.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
       {
         image: "images/placeholders/placeholder-portfolio-image.png",
-        description: "Description",
+        description: "Nulla culpa anim do aliqua aute fugiat excepteur fugiat consectetur deserunt sint consectetur eu reprehenderit in laboris.",
         link: {
           github: "http://www.github.com",
           demo: "http://www.jonbalon.io"
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
       {
         image: "images/placeholders/placeholder-portfolio-image.png",
-        description: "Description",
+        description: "Deserunt culpa adipisicing magna quis qui amet labore ad incididunt.",
         link: {
           github: "http://www.github.com",
           demo: "http://www.jonbalon.io"
@@ -27,7 +27,7 @@ $(document).ready(function() {
 
       {
         image: "images/placeholders/placeholder-portfolio-image.png",
-        description: "Description",
+        description: "Sit sunt duis pariatur velit esse amet qui sint enim eiusmod incididunt fugiat ex.",
         link: {
           github: "http://www.github.com",
           demo: "http://www.jonbalon.io"
