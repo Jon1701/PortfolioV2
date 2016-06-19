74cf2ca64aa7753d0b12cf446c84400bc71e3a40
Jon Balon <development@jonbalon.com>
2016-06-19 03:53:25 -0400

    Added smooth scrolling effect, added navbar background change effect

--- source/js/effects.js
+++ source/js/effects.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+
   function enter() {
     $(this).children("span").css("visibility", "visible");
   }
@@ -9,5 +10,43 @@ $(document).ready(function() {
   }
 
   $(".navbar-nav > li > a").hover(enter, leave);
+
+
+
+  $(window).scroll(function() {
+
+    if ($(document).scrollTop() > 568) {
+      $("#navbar-main").removeClass("navbar-transparent-bg");
+      $("#navbar-main").addClass("navbar-lightdark-bg");
+    } else {
+      $("#navbar-main").removeClass("navbar-lightdark-bg");
+      $("#navbar-main").addClass("navbar-transparent-bg");
+    }
+
+  });
+
+
+
+
+
+
+  // Smooth scrolling.
+  $("#navbar-main > li > a").on("click", function(e) {
+
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 500, 'swing');
+
+  });
+
+
+
+
+
 
 });
