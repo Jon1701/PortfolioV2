beed5f2b1a86c0810be59703cca9cd22f1f21408
Jon Balon <development@jonbalon.com>
2016-06-20 01:43:53 -0400

    Renamed many of the folders (css-->stylesheets, js-->javascript) to make folder names more meaningful, modified all tasks to reflect this change, rearranged existing tasks, modified all tasks to recursively search folders, added more watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -16,22 +16,35 @@ var modulesPath = "node_modules/";
 // Tasks
 ////////////////////////////////////////////////////////////////////////////////
 
-// Move HTML files.
-gulp.task("html", function() {
-  gulp.src(srcPath + "*.html")
-    .pipe(gulp.dest(destPath));
-});
+// Move fonts.
+gulp.task("fonts", function() {
+  gulp.src(srcPath + "fonts/**/*")
+    .pipe(gulp.dest(destPath + "fonts/"));
+})
 
 // Move images.
 gulp.task("images", function() {
-  gulp.src(srcPath + "img/**/*")
+  gulp.src(srcPath + "images/**/*")
     .pipe(gulp.dest(destPath + "images/"));
 });
 
 // Move JavaScript.
 gulp.task("javascript", function() {
-  gulp.src(srcPath + "js/*.js")
-    .pipe(gulp.dest(destPath + "js"));
+  gulp.src(srcPath + "javascript/**/*.js")
+    .pipe(gulp.dest(destPath + "javascript/"));
+});
+
+// Compile and move stylesheets.
+gulp.task("stylesheets", function() {
+  gulp.src(srcPath + "stylesheets/**/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest(destPath + "stylesheets/"));
+});
+
+// Move HTML files.
+gulp.task("html", function() {
+  gulp.src(srcPath + "*.html")
+    .pipe(gulp.dest(destPath));
 });
 
 // Move Components.
@@ -57,22 +70,9 @@ gulp.task("components", function() {
 
 });
 
-// Compile and move .scss.
-gulp.task("scss", function() {
-  gulp.src(srcPath + "scss/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest(destPath + "css"));
-});
-
-// Fonts.
-gulp.task("fonts", function() {
-  gulp.src(srcPath + "fonts/**/*")
-    .pipe(gulp.dest(destPath + "fonts/"));
-});
-
 // Webserver.
 gulp.task("webserver", function() {
-  gulp.src("./build")
+  gulp.src(destPath)
     .pipe(webserver({
       fallback: "index.html",
       livereload: true,
@@ -87,14 +87,15 @@ gulp.task("webserver", function() {
 
 // Watch task
 gulp.task("watch", function() {
+  gulp.watch(srcPath + "fonts/**/*", ["fonts"]); // Fonts.
+  gulp.watch(srcPath + "images/**/*", ["images"]); // Images.
+  gulp.watch(srcPath + "javascript/**/*", ["javascript"]); // JavaScript.
+  gulp.watch(srcPath + "stylesheets/**/_*.scss", ["stylesheets"]); // SASS Partials.
+  gulp.watch(srcPath + "stylesheets/**/*.scss", ["stylesheets"]); // SASS Main.
   gulp.watch(srcPath + "*.html", ["html"]); // HTML files.
-  gulp.watch(srcPath + "img/backgrounds/*", ["images"]); // Images.
-  gulp.watch(srcPath + "js/*.js", ["javascript"]); // JavaScript.
-  gulp.watch(srcPath + "scss/_*.scss", ["scss"]); // SASS Partials.
-  gulp.watch(srcPath + "scss/*.scss", ["scss"]); // SASS Main.
 });
 
 ////////////////////////////////////////////////////////////////////////////////
 // Default Task.
 ////////////////////////////////////////////////////////////////////////////////
-gulp.task("default", ["watch", "html", "scss", "images", "javascript" ,"components", "fonts", "webserver"]);
+gulp.task("default", ["fonts", "images", "javascript", "stylesheets", "html", "components", "webserver"]);
