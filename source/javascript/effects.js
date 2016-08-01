dac9dab6daae09e68fc53fe5e283efb53369cd49
Jon Balon <development@jonbalon.com>
2016-08-01 02:34:04 -0400

    Removed existing navbar

--- source/javascript/effects.js
+++ source/javascript/effects.js
@@ -1,79 +1,6 @@
 $(document).ready(function() {
 
   //////////////////////////////////////////////////////////////////////////////
-  // Navbar toggle
-  //////////////////////////////////////////////////////////////////////////////
-  $("#navbar-main .handle").on("click", function() {
-    $("#navbar-main ul").toggleClass("showing");
-  });
-
-  //////////////////////////////////////////////////////////////////////////////
-  // Navbar background effect
-  //////////////////////////////////////////////////////////////////////////////
-
-  // Add translucent background if the menu is expanded.
-  //
-  // Initially the user will be on the Home section, which has a light
-  // background, and cannot see the menu if on mobile.
-  $(".handle").on("click", function() {
-
-    if ($("ul").hasClass("showing")) {
-      $("ul").removeClass("navbar-bg-transparent");
-      $("ul").addClass("navbar-bg-translucent");
-    }
-
-  });
-
-  // Effect triggers when window scrolls.
-  $(window).scroll(function() {
-
-    // When scrolled more than 100px from the top,
-    if ($(document).scrollTop() > 100) {
-
-      // Remove transparent background.
-      $("#navbar-main ul").removeClass("navbar-bg-transparent");
-      $(".handle").removeClass("navbar-bg-transparent");
-
-      // Apply a light dark background.
-      $("#navbar-main ul").addClass("navbar-bg-translucent");
-      $(".handle").addClass("navbar-bg-translucent");
-
-    } else { // When not scrolled more than 100 from the top,
-
-      // Remove light dark background.
-      $("#navbar-main ul").removeClass("navbar-bg-translucent");
-      $(".handle").removeClass("navbar-bg-translucent");
-
-      // Apply transparent background.
-      $("#navbar-main ul").addClass("navbar-bg-transparent");
-      $(".handle").addClass("navbar-bg-transparent");
-
-    }
-
-  });
-
-  //////////////////////////////////////////////////////////////////////////////
-  // Navbar link jump effect.
-  //////////////////////////////////////////////////////////////////////////////
-
-  // When a navbar link is clicked,
-  $("#navbar-main a").on("click", function(event) {
-
-    // Do not immediately go to the desired section.
-    event.preventDefault();
-
-    // Get the link anchor.
-    var target = this.hash;
-    var $target = $(target);
-
-    // Smooth transition to the section.
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-    }, 500, 'swing');
-
-  });
-
-  //////////////////////////////////////////////////////////////////////////////
   // Portfolio Project Description Popouts.
   //////////////////////////////////////////////////////////////////////////////
 
@@ -120,8 +47,5 @@ $(document).ready(function() {
     });
 
   });
-
-
-
 
 });
